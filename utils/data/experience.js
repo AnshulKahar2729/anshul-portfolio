c152b553d7e5586c9e94056287f6dff92bf40d35
Anshul Kahar <22511126.dypit@dypvp.edu.in>
2024-03-15 23:08:07 +0530

    FIRST COMMIT

---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Full Stack Web Developer',
+    company: "MavySoft Private Ltd.",
+    duration: "(Jan 2022 - Apr 2022)"
+  },
+ /*  {
+    id: 2,
+    title: "FullStack Developer",
+    company: "Fiverr (freelance)",
+    duration: "(Jun 2021 - Jan 2022)"
+  },
+  {
+    id: 3,
+    title: "Self Employed",
+    company: "Code and build something in everyday.",
+    duration: "(Jan 2018 - Present)"
+  } */
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Anshul Kahar",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Anshul Kahar. I am a full stack web developer. I build full stack web applications using MERN stack. I am a quick learner and always ready to learn new technologies. I am passionate about coding and solving problems. I am a team player and always ready to help others. I am a good communicator",
+  email: 'anshulkahar2211@gmail.com',
+  phone: '+91 7020004857',
+  address: 'Pune, India',
+  github: 'https://github.com/AnshulKahar2729',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/anshul-kahar/',
+  twitter: 'https://twitter.com/AnshulKahar2729',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://docs.google.com/document/d/1TO_Kl_srNu0UTtQMrMA69AIRkLjATthF0Jndr60pauA/edit"
+}
